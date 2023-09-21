d2c30c3d040750ccf3fba8209cb1e312b9c059b0
Ádám Bukta <90989331+AdamBukta@users.noreply.github.com>
2023-09-21 08:37:40 +0200

    változók létrehozása (easy-mode.js)

--- website/easy-mode.js
+++ website/easy-mode.js
@@ -1,1 +1,11 @@
+// KÖNNYŰ JÁTÉK
+// 3 üzenet(nagyobb vagy kisebb, tippek száma, korábbi tippek)
+var msg1 = document.getElementById("message1");
+var msg2 = document.getElementById("message2");
+var msg3 = document.getElementById("message3");
 
+// véletlen szám generálása 1 és 10 között
+var answer = Math.floor(Math.random()*10) + 1;
+//tippek és tippek számának tárolása
+var guessed_nums = [];
+var no_of_guesses = 0;
