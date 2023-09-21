303386643e652dab7f9dd54d2f6226ff8f486a15
Dino Akos <akosdino87@gmail.com>
2023-09-21 19:01:21 +0200

    Egységesítés

--- website/main.js
+++ website/main.js
@@ -1,10 +1,4 @@
-//megkeresi a startgombot és megnyomásra megnyitja a game oldalt
-let starter = document.getElementById("starter");
-starter.onclick = function () {
-    window.open("./game.html", "_self")
-}
-
-function redirectToPage() {
-    window.location.href = "./easy.html";
+function redirectToPage(y) {
+    window.location.href = y;
 }
 
