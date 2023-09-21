790a57b4791131065aff5a74e86626bc72f3ca9c
Dino Akos <akosdino87@gmail.com>
2023-09-21 04:47:46 +0200

    game html létrehozva, start gomb átirányít rá

---
+++ website/main.js
@@ -0,0 +1,6 @@
+//megkeresi a startgombot és megnyomásra megnyitja a game oldalt
+let starter = document.getElementById("starter");
+starter.onclick = function () {
+    window.open("./game.html", "_self")
+    
+}
