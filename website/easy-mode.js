d4a814ed75fc8129cb1374421e8e7453970c8d4c
dinoakos <43711534+dinoakos@users.noreply.github.com>
2023-09-24 20:36:52 +0200

    scoreboard kódjának megírása

--- website/easy-mode.js
+++ website/easy-mode.js
@@ -30,6 +30,8 @@ switch(difficulty){
     
 }
 
+
+
 //tippek és tippek számának tárolása
 var guessed_nums = [];
 var no_of_guesses = 0;
@@ -60,68 +62,11 @@ function play(){
             msg1.textContent = "Nyertél!";
             msg2.textContent = "A szám " + answer + " volt.";
             msg3.textContent =  no_of_guesses + " tipp által találtad ki.";
+            localStorage.setItem("localGuess", no_of_guesses);
             document.getElementById("my_btn").disabled = true;
-            document.getElementById("scorescreen").style.visibility="visible";      //játék vége
+            document.getElementById("scorescreen").style.visibility="visible";
+            
+            //játék vége
         }
     }
 }
-function convert() {
-           
-    // Sample JSON data
-    let jsonData = [
-       {
-          name: "Saurabh",
-          age: "20",
-          city: "Prayagraj"
-       },
-       {
-          name: "Vipin",
-          age: 23,
-          city: "Lucknow",
-       },
-       {
-          name: "Saksham",
-          age: 21,
-          city: "Noida"
-       }
-    ];
-    
-    // Get the container element where the table will be inserted
-    let container = document.getElementById("container");
-    
-    // Create the table element
-    let table = document.createElement("table");
-    
-    // Get the keys (column names) of the first object in the JSON data
-    let cols = Object.keys(jsonData[0]);
-    
-    // Create the header element
-    let thead = document.createElement("thead");
-    let tr = document.createElement("tr");
-    
-    // Loop through the column names and create header cells
-    cols.forEach((item) => {
-       let th = document.createElement("th");
-       th.innerText = item; // Set the column name as the text of the header cell
-       tr.appendChild(th); // Append the header cell to the header row
-    });
-    thead.appendChild(tr); // Append the header row to the header
-    table.append(tr) // Append the header to the table
-    
-    // Loop through the JSON data and create table rows
-    jsonData.forEach((item) => {
-       let tr = document.createElement("tr");
-       
-       // Get the values of the current object in the JSON data
-       let vals = Object.values(item);
-       
-       // Loop through the values and create table cells
-       vals.forEach((elem) => {
-          let td = document.createElement("td");
-          td.innerText = elem; // Set the value as the text of the table cell
-          tr.appendChild(td); // Append the table cell to the table row
-       });
-       table.appendChild(tr); // Append the table row to the table
-    });
-    container.appendChild(table) // Append the table to the container element
- }
